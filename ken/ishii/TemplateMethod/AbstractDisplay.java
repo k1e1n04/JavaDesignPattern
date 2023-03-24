25302ab6b8998510d84060503d7230bf56c819f4
Ken Ishii <riguveda44@gmail.com>
2023-03-25 00:44:56 +0900

    TemplateMethod を追加

---
+++ ken/ishii/TemplateMethod/AbstractDisplay.java
@@ -0,0 +1,16 @@
+package ken.ishii.TemplateMethod;
+
+public abstract class AbstractDisplay {
+    public abstract void open();
+    public abstract void print();
+    public abstract void close();
+    //Template Method
+    public final void display() {
+        open();
+        for (int i =0; i < 3; i++) {
+            print();
+        }
+        close();
+    }
+
+}
